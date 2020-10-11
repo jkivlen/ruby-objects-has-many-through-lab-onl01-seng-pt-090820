595a65ab320b286df6d22b8b29ab0c692e8aab65
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-11 09:04:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  
+  @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+  
+  
+  
+  
+  def self.all
+    @@all
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
