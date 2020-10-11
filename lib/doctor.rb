73ddcc5b07d7253ae00fee3ff71abac27d5f6537
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-11 09:21:05 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, name)
+  
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
